ae8e4f3784543b11de3327a6435df8e554d36789
Arundathi Shaji Shanthini <arundathisanthini@gmail.com>
2020-01-26 17:35:17 +0000

    Adding the file for Activity 1

---
+++ Activity 1/comb_struct.sv
@@ -0,0 +1,26 @@
+/*
+The module defines a structural description of the circuit provided in Worksheet 1. 
+Author: Arundathi Shaji Shanthini
+*/
+module comb_struct(input logic A,B,C,D, output logic F);
+
+	wire w1,w2,w3,not_D,E;
+
+	and(w1,A,B);
+	not(E,B);	
+	and(w2,E,C);
+	not(not_D,D);
+	or(w3,w1,w2);
+	and(F,w3,not_D);
+
+endmodule
+
+// An example or reference:
+
+// Code that depicts defining a module object
+/* 
+module ADD_Workshop1_activity1();
+	logic A,B,C,D,F; 
+	comb_struct cs(A,B,C,D,F);
+endmodule
+*/
