67f5575e48832760e7044b93cdc6adb952de4f89
Arundathi Shaji Shanthini <arundathisanthini@gmail.com>
2020-03-03 10:45:39 +0000

    Tested and working version of Exercise 3_1 v2

--- Workshop_4/Exercise_3_1.asm
+++ Workshop_4/Exercise_3_1.asm
@@ -1,4 +1,4 @@
-# Exercise 3:  Generate Fibonacci Series - upto 20 terms
+# Exercise 3:  Generate Fibonacci Series
 # Given x = $s1 and y = $s2
 
 # Initialise terms
@@ -9,11 +9,11 @@ addi $s5,$zero, 20 # n (number of terms)
 addi $s3, $zero, 0 
 addi $s3, $zero, 1 
 addi $s4,$zero, 2 # i (loop control variable)
-while:
+loop:
 add $s3, $s2, $s1
 add $s1, $zero, $s2
 add $s2, $zero, $s3
 addi $s4,$s4,1 # Increment loop control variable by 1
 beq $s4,$s5, done # If i==y then go to branch done
-j while # Jump to while 
+j loop # Jump to while 
 done:
